9939ede9a1d58502efeb7f8311e567c9e51d8ae2
SRS <shaneradio@gmail.com>
2018-08-06 17:22:38 -0400

    continue building out reverseString2.js

--- reverseString2.js
+++ reverseString2.js
@@ -1,10 +1,11 @@
-// challenge: for any given string, write a script to return its reverse
-// (a string with the same characters in the given string,
-// but in reverse order)... this time, we'll avoid using the built-in
-// reverse method foir arrays
+// challenge: for any given string, write a script to return its
+// "reverse" (a string with the same characters in the given string,
+// but in reverse order)... this time, we'll avoid using the
+// built-in reverse method for arrays
 
 function reverse (str) {
-    var rev = str.split('').reverse().join('');
+    var rev = '';
+    for 
     // return rev;
     console.log(rev);
 }
@@ -18,4 +19,4 @@ function reverse (str) {
 
 // below: example use of function
 
-reverse("everyday");
+reverse("tomorrow");
