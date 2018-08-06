1e00c02171f2e24a72dd9ba91e4c9de844ad2bec
SRS <shaneradio@gmail.com>
2018-08-06 17:41:38 -0400

    dummy file for reverseString3.js

---
+++ reverseString3.js
@@ -0,0 +1,22 @@
+// challenge: for any given string, write a script to return its reverse
+// (a string with the same characters in the given string,
+// but in reverse order)
+
+// example function call is with string "everyday"
+
+function reverse (str) {
+    var rev = str.split('').reverse().join('');
+    // return rev;
+    console.log(rev);
+}
+
+// above: splits given string into array of individual characters,
+// using empty-string separator, uses reverse method for array,
+// and joins the reversed array of characters,
+// creating string "reversal"
+
+// ====================================
+
+// below: example use of function
+
+reverse("everyday");
