d14f3213027eacef06dc1b760be5501b489288a3
SRS <shaneradio@gmail.com>
2018-08-06 17:11:21 -0400

    one piece

---
+++ reverseString1.js
@@ -0,0 +1,11 @@
+// challenge: for any given string, write a script to return its reverse
+// (a string with the same characters in the given string,
+// but in reverse order)
+
+reverse (str) => {
+    const rev = string.split('').reverse().join('');
+    return rev;
+    console.log(rev);
+}
+
+reverse("everyday");
