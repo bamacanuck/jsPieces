1ba3f362c367cced7c696e44474dc721460a5c48
S.R. Scott <shaneradio@gmail.com>
2018-08-07 18:13:42 -0400

    palindrome check 1 now yielding correct node output...

--- checkPalindrome1.js
+++ checkPalindrome1.js
@@ -1,19 +1,18 @@
 // challenge: for any given string, write a script that will return
 // true if it's a palindrome (the same with the characters reversed)
 
-// example function call is with string "abba"
+// example function calls are with strings "abba" and "abbc"
 
 function palinCheck (str) {
   let rev = str.split('').reverse().join('');
 
   if (str === rev) {
-		return true;
-		console.log("true");
+		console.log(true);
   }
   else {
-		return false;
-		console.log("false");
+		console.log(false);
   }
 }
 
 palinCheck("abba");
+palinCheck("abbc");
