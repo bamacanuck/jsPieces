075fcb14ffa411fce6c2e4952c69f80af37b0a33
S.R. Scott <shaneradio@gmail.com>
2018-08-08 13:49:34 -0400

    comments on finished reverseInteger1

--- reverseInt1.js
+++ reverseInt1.js
@@ -9,12 +9,29 @@
 // -13 input returns -31
 // -700 input returns -7
 
-// example function call below is with int 35486
+// example function call below is with int -0000068453
 
 function reverseInt (int) {
     rev = int.toString().split('').reverse().join('');
     rev = parseInt(rev);
+    rev = (rev * (Math.sign(int)));
     console.log(rev);
 }
 
-reverseInt(35486);
+reverseInt(-0000068453);
+
+// =====================================
+
+// above: given integer int is made a string, and split into an array
+// of individual characters... the array is then reversed, and
+// the characters joined to form a reversed string version...
+// this string is then made an integer, and this is multiplied by
+// either one, negative one, or zero, depending on the sign (or
+// lack thereof) of the original given integer - int 
+
+// =====================================
+
+// THERE ARE HIDDEN (AND UNIDENTIFIED) BUGS, OR AT LEAST
+// ONE SUCH BUG...
+// reverseInt(-000000000001000000000000000000068453) yields
+// -42, for instance
