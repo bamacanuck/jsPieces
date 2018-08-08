e1e093966b2a3d19180a77635205a28be9aa49bd
S.R. Scott <shaneradio@gmail.com>
2018-08-08 12:22:54 -0400

    working through logic for reverseInt1

--- reverseInt1.js
+++ reverseInt1.js
@@ -1,8 +1,14 @@
 // challenge: for any given integer, write a script that will return
 // its reverse (integer with same digits, in reverse order)
 
-// 013 input returns 310
+// 013 input returns 31 - NOT 310
 // 455 input returns 554
 
 // example function call below is with int 35486
 
+function reverseInt (int) {
+    rev = int.toString().split('').reverse().join('').parseInt();
+    console.log(rev);
+}
+
+reverseInt(35486);
