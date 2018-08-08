c67669b3d5d4a9f739c7c235c0ccbe8854223b15
S.R. Scott <shaneradio@gmail.com>
2018-08-08 13:57:04 -0400

    description for pluralityChar.js

---
+++ pluralityChar.js
@@ -0,0 +1,9 @@
+// challenge: for any given string, write a script that will return
+// the character that appears most within it
+
+// "Matthew" input returns "t"
+// "Willis" input returns "i" and "l" (?)
+// "1111s111122211111s111" input returns 1
+
+// example function call below is with string "ABBBA"
+
