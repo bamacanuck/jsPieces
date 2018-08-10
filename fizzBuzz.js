e26be7eca4ac74094ea833e1386329ae6f635547
S.R. Scott <shaneradio@gmail.com>
2018-08-10 18:25:16 -0400

    logic on track for fizzBuzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,10 +1,14 @@
-// challenge: the classic "fizzbuzz" -
+// challenge: the classic "FizzBuzz" -
 // for any given integer, write a script that logs the
-// integers from 1 to it... but instead logs "fizz" for
-// multiples of three, "buzz" for multiples of five,
-// and “fizzbuzz” for those integers that are multiples
+// integers from 1 to it... but instead logs "Fizz" for
+// multiples of three, "Buzz" for multiples of five,
+// and “FizzBuzz” for those integers that are multiples
 // of both three AND five
 
 function fizzBuzz (x) {
-    
+    for (let i = 1; i <= x; i++) {
+        if (i%15 = 0) {
+            console.log("FizzBuzz");
+        }
+    }
 }
