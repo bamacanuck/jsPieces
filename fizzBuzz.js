ceeb7f091d0c5f858a85a86cdd4b141c5879cf9b
S.R. Scott <shaneradio@gmail.com>
2018-08-10 18:29:34 -0400

    fizzBuzz logic complete (?)

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -10,5 +10,16 @@ function fizzBuzz (x) {
         if (i%15 = 0) {
             console.log("FizzBuzz");
         }
+        else if ((i%5 = 0) && (i%3 != 0)) {
+            console.log("Fizz");
+        }
+        else if ((i%3 = 0) && (i%5 != 0)) {
+            console.log("Buzz");
+        }
+        else {
+            console.log(i);
+        }
     }
-}
+};
+
+fizzBuzz(50);
