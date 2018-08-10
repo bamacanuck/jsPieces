190f060761de2fffa994dc9fc8b622c6fbe34a2d
S.R. Scott <shaneradio@gmail.com>
2018-08-10 18:15:47 -0400

    working on fizzBuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,7 @@
+// challenge: the classic "fizzbuzz" -
+// for any given integer, write a script that logs the
+// integers from 1 to it... but instead logs "fizz" for
+// multiples of three, "buzz" for multiples of five,
+// and “fizzbuzz” for those integers that are multiples
+// of both three AND five
+
