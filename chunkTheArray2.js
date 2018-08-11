a61bb6730812550adf4737008ac4c9242c47d87f
Shane Scott <shaneradio@gmail.com>
2018-08-11 13:00:10 -0400

    chunkTheArray second version

---
+++ chunkTheArray2.js
@@ -0,0 +1,12 @@
+// challenge: for any given array and a
+// "chunk" size, divide the array into
+// subarrays, with each of them of the
+// specified "chunk"-length (and whatever
+// is left over, in a final subarray)
+
+// ([1, 2, 3, 4, 5, 6], 2) yields [[1, 2], [3, 4], [5, 6]]
+// ([1, 2, 3, 4, 5, 6], 3) yields [[1, 2, 3], [4, 5, 6]]
+// ([2, 4, 6, 8, 10, 12, 9, 5], 5) yields
+// [[2, 4, 6, 8, 10], [12, 9, 5]]
+// ([2, 4, 8, 62, 17], 5) yields [[2, 4, 8, 62, 17]]
+// ([2, 4, 8, 62, 17], 10) yields [[2, 4, 8, 62, 17]]
