4b20bcc926a150b9e6a911bf7dda176db436abd5
Shane Scott <shaneradio@gmail.com>
2018-08-11 13:11:56 -0400

    description for capTtitle

---
+++ capTitle.js
@@ -0,0 +1,9 @@
+//challenge: for any given multi-word string,
+// write a script to capitalize the first
+// letter of each word in it, and return the
+// new version with the capitalizations, having
+// the same spacing and punctuation
+
+// 'the great escape' yields 'The Great Escape'
+// 'michael clayton' yields 'Michael Clayton'
+// '' yields
