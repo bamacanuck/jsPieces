d898e2c010e1af0ff141f714852f54c6cffa0e16
S.R. Scott <shaneradio@gmail.com>
2018-08-18 21:52:28 -0400

    building pseudocode logic for chunking arrays...

--- chunkTheArray1.js
+++ chunkTheArray1.js
@@ -11,5 +11,25 @@
 // ([2, 4, 8, 62, 17], 5) yields [[2, 4, 8, 62, 17]]
 // ([2, 4, 8, 62, 17], 10) yields [[2, 4, 8, 62, 17]]
 
+
+//create new empty array
+
 const chunked = [];
 
+//for each item/element in original 'unchunked' array,
+
+****
+
+// get the last item/element in the new 'chunked' array
+
+****
+
+// if said element/item's place in the chunk array is of
+// length equal to the specified/desired chunk length,
+// OR if said element/item does not exist,
+
+// add a new inner chunk (or "sub-chunk") to the 'chunked'
+// array, containing the current iteration element/item
+
+// OTHERWISE - add the current iteration element/item to
+// the presently-forming "sub-chunk"
