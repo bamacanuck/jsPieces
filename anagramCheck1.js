6ca3b6146314c274540c541e46b9ade26ab600e5
Shane Scott <shaneradio@gmail.com>
2018-08-21 19:24:36 -0400

    added doc for second anagramCheck solution

---
+++ anagramCheck1.js
@@ -0,0 +1,38 @@
+// challenge: given any two strings, check
+// whether they are anagrams of each other...
+// disregarding spaces and punctuation marks...
+// with lower-case letters and capitals
+// considered equivalent ... (and matching
+// character quantities, OF COURSE, counts)
+
+// ('car has', 'a crash') yields True
+// anagrams('ELVIS!', 'Lives?') --> True
+// anagrams('gentleman', 'elegant man') --> False
+
+function anagramCheck (string1, string2) {
+
+}
+
+module.exports = anagramCheck;
+
+
+// consider char-plurality solution, noted below
+
+function pluralityChar (str) {
+	const charObj = {};
+	let most = 0;
+	let mostChar = '';
+
+  for (let char of str) {
+    charObj[char] = charObj[char] + 1 || 1;
+	}
+
+	// console.log(charObj);
+
+	for (let char in charObj) {
+		if (charObj[char] > most) {
+			most = charObj[char];
+			mostChar = char;
+			// console.log(char + ":" + charObj[char]);
+		}
+	}
