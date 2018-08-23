4f31c145137b06bdb23aca60bad0ed466198b0b3
S.R. Scott <shaneradio@gmail.com>
2018-08-22 22:43:39 -0400

    using char-plurality logic for anagramCheck1... ...

--- anagramCheck1.js
+++ anagramCheck1.js
@@ -14,6 +14,7 @@ function anagramCheck (string1, string2) {
 	// object to hold the involved characters
 	const charObj = {};
 
+	// iterate over char object
 	for (let char of str) {
 		charObj[char] = charObj[char] + 1 || 1;
 
