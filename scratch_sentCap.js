d8f0941baa60eafd5d2d24871a9c8ff2a31c791b
Shane Scott <shaneradio@gmail.com>
2018-08-25 22:49:24 -0400

    working the logic in scratch_sentCap.js

--- scratch_sentCap.js
+++ scratch_sentCap.js
@@ -1,12 +1,17 @@
-let str = "our puppy bear";
+let str = "that puppy bear";
 
 // let words = [];
 
 words = str.split(' ');
 
-for (let word in (words)) {
-  let cap = word.slice(0,1).toUpperCase();
-  let restWord = word.slice(1);
-  let newWord = cap + restWord;
-  return NewWord  
-}
+// for (let word in (words)) {
+//   let cap = word.slice(0,1).toUpperCase();
+//   let restWord = word.slice(1);
+//   newWord = cap + restWord;
+//   console.log(cap);  
+// }
+
+console.log(words[0].slice(0,1).toUpperCase());
+console.log(words[0].slice(1));
+
+// let cap = word.slice(0,1).toUpperCase();
