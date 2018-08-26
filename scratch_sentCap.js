69d36935f05a4e1976cc1fa209a2a16f07deb1a5
Shane Scott <shaneradio@gmail.com>
2018-08-25 23:06:52 -0400

    now have new array with desired word capitalization...

--- scratch_sentCap.js
+++ scratch_sentCap.js
@@ -4,15 +4,18 @@ let str = "that puppy bear";
 
 words = str.split(' ');
 
+let newArray = [];
+
 for (i = 0; i < words.length; i++) {
   let cap = words[i].slice(0,1).toUpperCase();
   let restWord = words[i].slice(1);
   newWord = cap + restWord;
-//   console.log(newWord);
-  let newSentence = [];
-  newSentence.push(newWord);
-  console.log(newSentence);
+	// console.log(newWord);
+	newArray.push(newWord);
 }
+
+
+  console.log(newArray);
 
 // console.log(words[i].slice(0,1).toUpperCase());
 // console.log(words[i].slice(1));
