6ba85846dde942ecdb339ca00b2ef05c4c464782
Shane Scott <shaneradio@gmail.com>
2018-08-30 22:03:35 -0400

    getting to work on alternate solution for sentcap

---
+++ sentenceCapitalize2.js
@@ -0,0 +1,10 @@
+// challenge: for any given string, write a script to return
+// a capitalized version, wherein the first letter of each of
+// its words is captalized, while preserving all
+// punctuation from the given string
+
+// input of "our puppy bear" should yield output of
+// "Our Puppy Bear"
+// and input of "tomorrow night, maybe" should yield output of
+// "Tomorrow Night, Maybe"
+
