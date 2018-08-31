1d3582725ac14ebb259305262c7675786f29acfc
S.R. Scott <shaneradio@gmail.com>
2018-08-31 15:13:51 -0400

    challenge written for printSteps.js

---
+++ printSteps.js
@@ -0,0 +1,20 @@
+// build a sort of visual representation of a staircase,
+// using pound signs - #, based on a given positive integer
+// ... such that the 'drawing' rendered has a number of
+// page lines equal to the integer, and a number of 
+// pound signs - # - equal to the line number
+
+// so, input: 3
+// yields
+// #
+// ##
+// ###
+
+// and input: 5
+// yields
+// #
+// ##
+// ###
+// ####
+// #####
+
