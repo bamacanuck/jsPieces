84b31dd2d722f5d0a3fef51afb51ba99ea0e70bd
Shane Scott <shaneradio@gmail.com>
2018-08-30 22:01:47 -0400

    included sentcap1 suggested solution from other source, and fixed it... it had been yielding all-letters capitalization

--- sentenceCapitalize.js
+++ sentenceCapitalize.js
@@ -12,34 +12,34 @@
 
 // consider array slice method
 
-// function capitalize (str) {
-// 	let words = str.split(' ');
+function capitalize (str) {
+	let words = str.split(' ');
 
-// 	let newArray = [];
+	let newArray = [];
 
-// 	for (i = 0; i < words.length; i++) {
-// 		let cap = words[i].slice(0,1).toUpperCase();
-// 		let restWord = words[i].slice(1);
-// 		newWord = cap + restWord;
-// 		// console.log(newWord);
-// 		newArray.push(newWord);
-// 	}
+	for (i = 0; i < words.length; i++) {
+		let cap = words[i].slice(0,1).toUpperCase();
+		let restWord = words[i].slice(1);
+		newWord = cap + restWord;
+		// console.log(newWord);
+		newArray.push(newWord);
+	}
 
-// 		let newSentence =	newArray.join(' ');
-// 		console.log(newSentence);
-// }
+		let newSentence =	newArray.join(' ');
+		console.log(newSentence);
+}
 
 // in another form...
 //
- function capitalize (str) {
-		let words = [];
+//  function capitalize (str) {
+// 		let words = [];
 
-  	for (let word of str.split('')) {
-			words.push(word[0].toUpperCase() +
-				word.slice(1));
-		}
+//   	for (let word of str.split(' ')) {
+// 			words.push(word[0].toUpperCase() +
+// 				word.slice(1));
+// 		}
 
-			console.log(words.join(''));
-}
+// 			console.log(words.join(' '));
+// }
 
 capitalize("!!! our puppy ! bear!");
