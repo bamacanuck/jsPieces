b659b0a3e07fedf4c1be1182bd7301d17ff69e59
Shane Scott <shaneradio@gmail.com>
2018-09-04 18:20:06 -0400

    adding better-commented version...

--- printSteps1.js
+++ printSteps1.js
@@ -39,33 +39,33 @@ function printSteps (int) {
 
 printSteps(4);
 
+// ===================================
 
 // better-commented version of solution (below)
 // 
-// function printSteps (int) {
-// 	// iterate over "rows" equal to int
-// 	for (let i = 0; i < int; i++) {
-// 			let line = "";
+function printSteps (int) {
+	// iterate over "rows" equal to int
+	for (let i = 0; i < int; i++) {
+			let line = "";
 			
-// 			// iterate over columns necessary
-// 			// to be equal to int
-// 			for (let col = 0; col < int; col++) {
-// 					// if condition is met
-//					if (col <= i) {
-//					// add a pound sign
-// 					line = line + "#";
-// 				}
-// 				else {
-// 					// otherwise, add a space
-// 					line = line + " ";
-// 				}
-// 			}
-// 		console.log(line);
-// 	}
-// }
+			// iterate over columns necessary
+			// to be equal to int
+			for (let col = 0; col < int; col++) {
+					// if condition is met
+					if (col <= i) {
+					// add a pound sign
+					line = line + "#";
+				}
+				else {
+					// otherwise, add a space
+					line = line + " ";
+				}
+			}
+		console.log(line);
+	}
+}
 
-// printSteps(4);
-
+// ===================================
 
 
 // some foolish early musings (below)
