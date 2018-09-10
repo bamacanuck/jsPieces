ebb7c4d09d484cc8bed042a92ba2aef7d8bb0e42
Shane Scott <shaneradio@gmail.com>
2018-09-09 21:33:14 -0500

    added docs for pyramidBuild solutions

---
+++ pyramidBuild2.js
@@ -0,0 +1,17 @@
+// build a sort of visual representation of a pyramid,
+// using pound signs - #, based on a given positive integer
+// ... such that the 'drawing' rendered has a number of
+// page lines equal to the integer, and a number of 
+// pound signs - # - sufficient to create the
+// 'pyramid' visual
+
+// so, input: 3
+// yields
+// '  #  '
+// ' ### '
+// '#####'
+
+// and input: 1
+// yields just
+// '#'
+
