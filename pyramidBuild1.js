a1057191a4049018b11d728d12da8d11bb1618a0
Shane Scott <shaneradio@gmail.com>
2018-09-24 22:48:12 -0400

    working from array midpoint for pyramidBuild1.js

--- pyramidBuild1.js
+++ pyramidBuild1.js
@@ -20,7 +20,7 @@ function pyramidBuild (n) {
         let level = "";
 
         for (let col = 0; col < (2*n) -1; col++) {
-            
+            const mid = Math.floor(((2 * n) - 1))
         }
     }
 }
