666f5a58c453c4d927f105f64a6b89a088bcf5a9
Shane Scott <shaneradio@gmail.com>
2018-11-29 13:12:52 -0500

    ...

--- here.js
+++ here.js
@@ -1,29 +1,6 @@
 
 function reverse (str) {
-    let rev = "";
-    // for (char of str) {
-    //     rev = char + rev;
-    // }
-
-    console.log(str[0]);
-    rev = str[0] + rev;
-    console.log(rev);
-    console.log(str[1]);
-    rev = str[1] + rev;
-    console.log(rev);
-    console.log(str[2]);
-    rev = str[2] + rev;
-
-    console.log(rev);
-    // return rev;
+    
 }
 
 reverse ("abc");
-
-
-
-// function reverse (str) {
-//     let rev = str.split('').reverse().join('');
-//     console.log(rev);
-//     return rev;
-// }
