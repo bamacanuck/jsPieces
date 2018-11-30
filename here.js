96b18fdfae94c6986a8a575c320a1cece50b3249
Shane Scott <shaneradio@gmail.com>
2018-11-30 15:27:06 -0500

    re-making revereseInt... with here.js as 'scratch'

--- here.js
+++ here.js
@@ -1,13 +1,7 @@
 
-function reverse (str) {
-    console.log(str.split('').reduce((rev, char) => {
-        return char + rev;        
-    }, '')
-    )
-    return (str.split('').reduce((rev, char) => {
-        return char + rev;        
-    }, '')
-    )
+function revInt (int) {
+
+    console.log(revdInt);
 }
 
-reverse ("abc");
+reverse ("-610");
